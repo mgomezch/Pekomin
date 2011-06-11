df9149049d67f36bde1be9157785711a65a0d364
Daniel <fleadanielito007@gmail.com>
2011-06-11 15:29:32 -0430

    arreglos pendientes revisar

--- KinematicFlee.hpp
+++ KinematicFlee.hpp
@@ -1,21 +1,22 @@
 #ifndef _KINEMATICFLEE_HPP
 #define _KINEMATICFLEE_HPP
 
-#include "KinematicV.hpp"
+#include "DirectKinematicV.hpp"
 
 using namespace std;
 
 class Ent;
 
-class KinematicFlee : public KinematicV {
+class KinematicFlee : public DirectKinematicV {
         public:
                 Ent *character;
                 Ent *target;
                 double maxSpeed;
+		double fleeRadius;
 
-                KinematicFlee(Ent *character, Ent *target, double maxSpeed);
+                KinematicFlee(Ent *character, Ent *target, double maxSpeed, double fleeRadius);
 
-                virtual pair<bool, Triple> getVelIncr(unsigned int ticks);
+                virtual pair<bool, Triple> getVel(unsigned int ticks);
 };
 
 #endif
--- KinematicSeek.hpp
+++ KinematicSeek.hpp
@@ -1,13 +1,13 @@
 #ifndef _KINEMATICSEEK_HPP
 #define _KINEMATICSEEK_HPP
 
-#include "KinematicV.hpp"
+#include "DirectKinematicV.hpp"
 
 using namespace std;
 
 class Ent;
 
-class KinematicSeek : public KinematicV {
+class KinematicSeek : public DirectKinematicV {
         public:
                 Ent *character;
                 Ent *target;
@@ -15,7 +15,7 @@ class KinematicSeek : public KinematicV {
 
                 KinematicSeek(Ent *character, Ent *target, double maxSpeed);
 
-                virtual pair<bool, Triple> getVelIncr(unsigned int ticks);
+                virtual pair<bool, Triple> getVel(unsigned int ticks);
 };
 
 #endif
